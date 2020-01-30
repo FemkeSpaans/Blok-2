04fa03ee3d63eae2531960c9c35e000d8f098f5a
FemkeSpaans <55233583+FemkeSpaans@users.noreply.github.com>
2020-01-30 17:37:31 +0100

    Add files via upload
    5de upload

--- Tentamen2.py
+++ Tentamen2.py
@@ -7,17 +7,15 @@ import re
 import numpy as np
 import matplotlib.pyplot as plt
 from tkinter import *
-from tkinter import messagebox
 
 
 def main():
-    name_file = "TAIR10_pep_20101214.fa"
-    name_file_gff3 = "TAIR10_GFF3_genes.gff"
-    headers, seqs = read_file_fasta(name_file)
-    matches = consensus(headers, seqs)
-    entry_match = open_file(name_file_gff3, matches)
-    one, two, three, four, five = matplotlib_data(entry_match)
-    matplotlib_graph(one, two, three, four, five)
+    #headers, seqs = read_file_fasta(name_file)
+    #matches = consensus(headers, seqs)
+    #entry_match = open_file(name_file_gff3, matches)
+    MyGUI()
+    #one, two, three, four, five = matplotlib_data(entry_match)
+    #matplotlib_graph(one, two, three, four, five)
 
 
 def read_file_fasta(name_file):
@@ -74,9 +72,9 @@ def consensus(headers, seqs):
 
 def open_file(name_file_gff3, matches):
     """
+    open file gff3 and put into a list
     :param: name_file_gff3
-    :param: matches
-    :return: entry_match
+    :return: protein
     """
     file = open(name_file_gff3, 'r')
     protein = []
@@ -89,6 +87,9 @@ def open_file(name_file_gff3, matches):
                 if re.search(r"ID=", entry):
                     if re.search(r"protein", entry):
                         entry_match.append(entry)
+    print("Gff3 ready")
+
+    file.close()
     return entry_match
 
 
@@ -122,9 +123,10 @@ def matplotlib_data(entry_match):
         elif re.search(r"Chr5", line):
             five += 1
         elif re.search(r"ChrC", line):
-            c +=1
+            c += 1
         elif re.search(r"ChrM", line):
-            m +=1
+            m += 1
+    print("Data graph ready")
     return one, two, three, four, five
 
 
@@ -148,16 +150,111 @@ def matplotlib_graph(one, two, three, four, five):
     plt.bar(range(len(names)), names)
     plt.show()
 
+
+# NIEUWE FUNCTIES
+def zoekLengteGen(entry_match):
+    start = int(entry_match[3])
+    end = int(entry_match[4])
+    length = end - start
+    return str(length)
+
+def count_amount_exons(name_file_gff3, match):
+    file = open(name_file_gff3, 'r')
+    exon = 0
+    protein = []
+    for line in file:
+        protein.append(line.strip().split("\t"))
+    for entry in protein:
+        if match in entry[8]:
+            if entry[2] == "exon":
+                exon += 1
+    file.close()
+    return exon
+
+
+
 class MyGUI:
     """
-
     """
-    root = Tk()
-    frame_buttons = Frame(root)
-    root.title("Accession numbers")
-    for item in entry_match
 
-    root.mainloop()
+    def __init__(self):
+        self.data = Data()
+
+        root = Tk()
+        root.title("huppeldepup")
+
+        mainframe = Frame(root, borderwidth=20)
+        mainframe.grid(row=0, column=0)
+
+        # label with accession numbers
+        label_dropdown = Label(mainframe, text="Accession numbers")
+        label_dropdown.grid(row=0, column=0, sticky="nw")
+
+        # dropdown menu
+        option_list = self.data.matches
+        variable = StringVar(root)
+        variable.set(option_list[0])
+        drop_down = OptionMenu(mainframe, variable, *option_list, command=self.data_display)
+        drop_down.grid(row=1, column=0, sticky="nw")
+
+        # label with extra information
+        label_info = Label(mainframe, text="Information")
+        label_info.grid(row=0, column=1, sticky="w", padx=(20, 0))
+
+        # text with information
+        self.text_chromo = Text(mainframe, width=50, height=5, state="disabled")
+        self.text_chromo.grid(row=1, column=1, sticky="w", padx=(20, 0))
+
+        # label visualisation
+        label_vis = Label(mainframe, text="Visualisation")
+        label_vis.grid(row=2, column=0, sticky="nw", pady=(10, 0))
+
+        # canvas for visualisation
+        picture_chromo = Text(mainframe, width=68, height=3)
+        picture_chromo.grid(row=3, column=0, columnspan=2, sticky="nw")
+
+
+        #chromosome_number = (entry_match[0])
+
+
+
+
+        root.mainloop()
+
+    def data_display(self, state):
+        # chromosome number finder
+        accession_number = self.data.matches.index(state)
+        data_entry = self.data.entry_match[accession_number]\
+            .strip().split("\t")
+        chromosome_number = data_entry[0][3]
+        text = "Chromosome number: " + chromosome_number
+        # length of the gene
+        length = zoekLengteGen(data_entry)
+        text_length = "Length of the gene: " + length
+        # count amount of exons
+        amount = count_amount_exons(self.data.name_file_gff3, state)
+        text_exons = "Amount of exons on this gene: " + str(amount)
+
+        # reset state so it can be adjusted,
+        # wipe text field clean,
+        # set state to disabled so it cannot be changed.
+        self.text_chromo.config(state="normal")
+        self.text_chromo.delete("1.0", "end")
+        self.text_chromo.insert("end", text + "\n" + text_length\
+                                + "\n" + text_exons)
+        self.text_chromo.config(state="disabled")
+
+
+
+class Data:
+    def __init__(self):
+        self.name_file = "TAIR10_pep_20101214.fa"
+        self.name_file_gff3 = "TAIR10_GFF3_genes.gff"
+
+        self.headers, self.seqs = read_file_fasta(self.name_file)
+        self.matches = consensus(self.headers, self.seqs)[0:5]
+        self.entry_match = open_file(self.name_file_gff3, self.matches)
+        print(self.entry_match)
 
 
 
